37bfa1b3a3a7e8c5484743a60bf6708b38faecc3
LauricioX <developerwebmasterpro@gmail.com>
2022-11-28 10:57:37 -0300

    ddd

--- frontend/index.js
+++ frontend/index.js
@@ -3,11 +3,7 @@ axios
   .then((response) => {
     let responser = document.getElementById("responser");
     response.data.forEach((data) => {
-      let res = document.createElement("h1");
-      res.innerHTML = `
-      <p>${data.nome}</p>
-      `;
-      responser.appendChild(res);
+      console.log(data);
     });
   })
   .catch((erro) => {
