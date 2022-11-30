fe4cdf12cd6ee17022822be88c6943cf2212bdea
LauricioX <developerwebmasterpro@gmail.com>
2022-11-30 10:16:05 -0300

    sddf

--- frontend/index.js
+++ frontend/index.js
@@ -1,32 +1,32 @@
-let myGames = [];
+function loadGames() {
+  axios
+    .get("http://localhost:4000/games")
+    .then((response) => {
+      let content = document.getElementById("content");
+      response.data.forEach((data) => {
+        let newData = document.createElement("tr");
+        newData.innerHTML = `
+        <td class="border">${data.nome}</td>
+        <td class="border bg-secondary text-white">${data.preco}</td>
+        <td class="border text-center" style="width: 90px">
+          <button class="rounded-3 bg-dark text-white border-2">
+            <i class="bi bi-trash3-fill"></i>
+          </button>
+          <button class="rounded-3 bg-dark text-white border-2">
+            <i class="bi bi-pencil-fill"></i>
+          </button>
+        </td>
+       `;
 
-axios
-  .get("http://localhost:4000/games")
-  .then((response) => {
-    let content = document.getElementById("content");
-    myGames.push(...response.data);
-    console.log(myGames);
-    myGames.forEach((data) => {
-      let newData = document.createElement("tr");
-      newData.innerHTML = `
-      <td class="border">${data.nome}</td>
-      <td class="border bg-secondary text-white">${data.preco}</td>
-      <td class="border text-center" style="width: 90px">
-        <button class="rounded-3 bg-dark text-white border-2">
-          <i class="bi bi-trash3-fill"></i>
-        </button>
-        <button class="rounded-3 bg-dark text-white border-2">
-          <i class="bi bi-pencil-fill"></i>
-        </button>
-      </td>
-     `;
-
-      content.prepend(newData);
+        content.prepend(newData);
+      });
+    })
+    .catch((erro) => {
+      console.log("erro");
     });
-  })
-  .catch((erro) => {
-    console.log(erro);
-  });
+}
+
+loadGames();
 
 //criando games
 
@@ -44,9 +44,12 @@ function criarGame() {
     .then((response) => {
       if ((response.status = 200)) {
         alert("game cadastrado com sucesso");
+        window.location.reload();
       }
     })
     .catch((erro) => {
       alert("erro ao cadastrar novo game");
     });
 }
+
+//deletando games
