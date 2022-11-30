aa3060d19b646426493b151614f8f3effdbc8afe
LauricioX <developerwebmasterpro@gmail.com>
2022-11-29 22:09:22 -0300

    ddd

--- frontend/index.js
+++ frontend/index.js
@@ -1,12 +1,16 @@
+let myGames = [];
+
 axios
   .get("http://localhost:4000/games")
   .then((response) => {
     let content = document.getElementById("content");
-    response.data.forEach((data) => {
+    myGames.push(...response.data);
+    console.log(myGames);
+    myGames.forEach((data) => {
       let newData = document.createElement("tr");
       newData.innerHTML = `
       <td class="border">${data.nome}</td>
-      <td class="border">${data.preco}</td>
+      <td class="border bg-secondary text-white">${data.preco}</td>
       <td class="border text-center" style="width: 90px">
         <button class="rounded-3 bg-dark text-white border-2">
           <i class="bi bi-trash3-fill"></i>
@@ -23,3 +27,26 @@ axios
   .catch((erro) => {
     console.log(erro);
   });
+
+//criando games
+
+function criarGame() {
+  let nome = document.getElementById("nome").value;
+  let preco = document.getElementById("preco").value;
+
+  let user = {
+    nome: nome,
+    preco: preco,
+  };
+
+  axios
+    .post("http://localhost:4000/games", user)
+    .then((response) => {
+      if ((response.status = 200)) {
+        alert("game cadastrado com sucesso");
+      }
+    })
+    .catch((erro) => {
+      alert("erro ao cadastrar novo game");
+    });
+}
